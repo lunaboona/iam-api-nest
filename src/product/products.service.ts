66b59816f0b8b8927d76da57ab12ed934d447e7d
Milena Milicio <milenamilicio@hotmail.com>
2022-04-30 20:00:32 -0300

    add structure for products

---
+++ src/product/products.service.ts
@@ -0,0 +1,12 @@
+import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { Product } from './entities/product.entity';
+
+@Injectable()
+export class ProductsService {
+  constructor(
+    @InjectRepository(Product)
+    private productsRepository: Repository<Product>,
+  ) {}
+}
