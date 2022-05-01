d7440693394fb01a0a271d187b1bcd15fd370871
Milena Milicio <milenamilicio@hotmail.com>
2022-04-30 21:26:36 -0300

    add product crud

--- src/product/products.module.ts
+++ src/product/products.module.ts
@@ -3,9 +3,15 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { ProductsController } from './products.controller';
 import { ProductsService } from './products.service';
 import { Product } from './entities/product.entity';
+import { ProductDefinitionsModule } from 'src/product-definitions/product-definitions.module';
+import { WarehousesModule } from 'src/warehouses/warehouses.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Product])],
+  imports: [
+    TypeOrmModule.forFeature([Product]),
+    ProductDefinitionsModule,
+    WarehousesModule,
+  ],
   controllers: [ProductsController],
   providers: [ProductsService],
   exports: [ProductsService],
