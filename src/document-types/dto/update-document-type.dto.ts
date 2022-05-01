c9861a0781b2bff4fcc99478dfe8f59ce5b4a346
lunaboona <hyaluna@gmail.com>
2022-05-01 15:39:11 -0300

    immutable document type

--- src/document-types/dto/update-document-type.dto.ts
+++
@@ -1,4 +0,0 @@
-import { PartialType } from '@nestjs/mapped-types';
-import { CreateDocumentTypeDto } from './create-document-type.dto';
-
-export class UpdateDocumentTypeDto extends PartialType(CreateDocumentTypeDto) {}
